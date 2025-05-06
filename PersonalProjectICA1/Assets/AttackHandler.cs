7c23b59dad3e73f555cc85ade46d38dcf77e017e
reykenj <230343s@mymail.nyp.edu.sg>
2025-05-06 20:54:27 +0800

    Start Of Spells

--- PersonalProjectICA1/Assets/AttackHandler.cs
+++ PersonalProjectICA1/Assets/AttackHandler.cs
@@ -1,21 +1,64 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AttackHandler : MonoBehaviour
 {
-    [SerializeField] float Damage;
-    [SerializeField] Collider Hitbox;
-    [SerializeField] float Cooldown;
+    [SerializeField] List<Spell> SpellArray;
+    public float CastTime = 0.25f;
+    public float timer;
+    public int Turn;
+
+    public bool isMainHandler = false;
+    public Transform AttackStartPoint;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ResetSpells();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+    public void Cast()
+    {
+        if (!isMainHandler) return;
+
+        for (int i = Turn; i < SpellArray.Count; i++)
+        {
+            Spell spell = SpellArray[i];
+            if (spell != null)
+            {
+                Turn++;
+                spell.Apply(SpellArray[Mathf.Clamp(i + 1, 0, SpellArray.Count - 1)], this, out bool UseTurn);
+                if (UseTurn)
+                {
+                    timer += CastTime;
+                    if (Turn >= SpellArray.Count)
+                    {
+                        Turn = 0;
+                    }
+                    ResetSpells(Turn, SpellArray.Count); // may need to change later if we gonna back spell wrapping (rune at end going to start cuz multicast)
+                    break;
+                }
+            }
+        }
+    }
+
+    private void ResetSpells(int Start, int End)
+    {
+        for (int i = Start; i < End; i++)
+        {
+            Spell spell = SpellArray[i];
+            spell.SpellReset();
+        }
     }
 }
---
+++ PersonalProjectICA1/Assets/ScriptableObjects/Spells/Spell.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spell", menuName = "Spells/New Spell")]
+public class Spell : ScriptableObject
+{
+    public ProjectileInformation OGProjectileInformation;
+    public ProjectileInformation TempProjectileInformation;
+    public string SpellName;
+    public Sprite RuneIcon;
+
+    // for when the user goes through all of the spells in the loop and is recharging, basically
+    // reset all modified values here
+    public virtual void SpellReset()
+    {
+        TempProjectileInformation = OGProjectileInformation;
+    }
+    public virtual void Apply(Spell NextSpell, AttackHandler attackHandler, out bool UseTurn)
+    {
+        UseTurn = false;    
+    }
+
+    public virtual void OnHit(Projectile projectile)
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public string perkName;
+//public string description;
+//public Sprite icon;
+//public bool isStackable;
+//public GameObject ModelPrefab;
+
+//protected Dictionary<string, GameObject> loadedPrefabs = new Dictionary<string, GameObject>();
+//public virtual PlayerStats ApplyEffect(GameObject player, int stackCount)
+//{
+//    Debug.Log($"Applying {perkName} with {stackCount} stacks.");
+//    PlayerStats statAdditions = new PlayerStats();
+//    return statAdditions;
+//}
+
+//public virtual void OnEventTrigger(GameObject player, int stackCount)
+//{
+//    Debug.Log($"{perkName} triggered with {stackCount} stacks.");
+//}
+//public virtual void EventSubscribe(GameObject player)
+//{
+//    Debug.Log($"{perkName} subscribed to {player.name}.");
+//}
+
+
+//protected void LoadPrefab(string prefabAddress, string prefabKey)
+//{
+//    Addressables.LoadAssetAsync<GameObject>(prefabAddress).Completed += (handle) =>
+//    {
+//        if (handle.Status == AsyncOperationStatus.Succeeded)
+//        {
+//            loadedPrefabs[prefabKey] = handle.Result;
+//            Debug.Log($"Prefab '{prefabAddress}' loaded successfully!");
+//        }
+//        else
+//        {
+//            Debug.LogError($"Failed to load prefab '{prefabAddress}'!");
+//        }
+//    };
+//}
+
+//public virtual void Init()
+//{
+//}
